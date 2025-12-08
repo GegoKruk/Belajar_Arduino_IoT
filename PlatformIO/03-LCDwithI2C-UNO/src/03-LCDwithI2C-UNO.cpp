89b593cf8555e51f5693352f882f3b3ba71cb984
Gega Ramadhan <gegaramadhan17ok@gmail.com>
2025-12-08 15:32:36 +0700

    Fix library LiquidCrystal_I2C by frank - project 03-LEDwithI2C-UNO - PlatformIO

--- PlatformIO/03-LCDwithI2C-UNO/src/03-LCDwithI2C-UNO.cpp
+++ PlatformIO/03-LCDwithI2C-UNO/src/03-LCDwithI2C-UNO.cpp
@@ -1,23 +1,31 @@
+#include <Arduino.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-
-LiquidCrystal_I2C lcd(PCF8574_ADDR_A21_A11_A01, 16, 2);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 void setup() {
   Wire.begin();       // aktifkan komunikasi I2C
-  lcd.begin(16, 2);   // inisialisasi LCD
+  lcd.init();         // inisialisasi LCD
   lcd.backlight();    // nyalakan lampu latar
 }
 
 
 void loop() {
-  lcd.setCursor(0, 0);
+  lcd.setCursor(0, 0);   // kolom 0, baris 0 → pojok kiri atas
   lcd.print("Halo Kamu!");
   delay(3000);
 
+  lcd.setCursor(0, 1);   // kolom 0, baris 0 
+  lcd.print("Selamat Siang!");
+  delay(3000);
+
   lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Selamat Pagi!");
+  lcd.setCursor(0, 0);   // kolom 0, baaris 1 
+  lcd.print("Semoga Sukses!");
+  delay(3000);
+
+  lcd.setCursor(5, 1);   // kolom 5, baris 1 → tengah bawah
+  lcd.print("Keren Keren");
   delay(3000);
 
   lcd.clear();
